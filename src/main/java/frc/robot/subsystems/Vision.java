467e3210eb31d0157212aabc459e8e25b922d09d
MadisonJohnston <58672198+MadisonJohnston@users.noreply.github.com>
2020-01-13 21:31:02 -0500

    Vision code being created
    set up the mode numbers 1-3
    set up the piplines 0-2, 9
    Created a getDoubleValue to get the value from strings

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -7,15 +7,58 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-public class Vision extends SubsystemBase {
   /**
-   * Creates a new ExampleSubsystem.
+   * The vision code using the limelight
+   * @author Madison J. and Zayd A.
    */
-  public Vision() {
+public class Vision extends SubsystemBase {
+  
+   public Vision() {
 
   }
+ /**
+   * Sets the LED mode to on, off, or blink
+   * @param mode - the mode of the LEDs
+   * Example: 
+   * 0: Sets the mode to what is in the current pipeline
+   * 1: Turns off the LEDs
+   * 2: Blink mode on LEDs
+   * 3: Turns on the LEDs
+   */
+
+   public void setLEDMode(int mode) {
+     NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(mode);
+   }
+/**
+ * Gets the Limelight mode number from the NetworkTable
+ * @return - returns the mode number from the NetworkTable 
+ */
+   public int getLEDMode() {
+     return (int)NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").getValue().getDouble();
+
+   }
+/**
+ * Sets the pipeline of the limelight
+ * @param pipeline - sets the desired pipeline number between 0-9
+ * 0 - CloseVision (0-15 ft away from the tower)
+ * 1 - MediumVision (between 15-21 ft)
+ * 2 - FarVision (21-26)
+ * 9 - Drivecam
+ */
+   public void switchPipeLine(int pipeline) {
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("pipeline").setNumber(pipeline);
+   }
+    /**
+     * This will get the value from tx, ta, etc. by using a string
+     * @param output - string double value
+     * @return - returns the double value from the string for example from ta, tx, etc.
+     */
+public double getDoubleValue(String output) {
+  return NetworkTableInstance.getDefault().getTable("limelight").getEntry(output).getDouble(0);
+}
+
 
   @Override
   public void periodic() {
