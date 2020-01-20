feca15d75087e7a90b46222936345086bc836e27
johnhasnocluehow <46582394+johnhasnocluehow@users.noreply.github.com>
2020-01-19 20:32:41 -0500

    Author statement added
    not much to it

--- src/main/java/frc/robot/subsystems/Hopper.java
+++ src/main/java/frc/robot/subsystems/Hopper.java
@@ -3,6 +3,11 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+/**
+ * @author John R.
+ * Function: Make variables for the two solenoids that operate the hopper system 
+ */
+
 public class Hopper extends SubsystemBase {
 
   // Solenoids being defined
