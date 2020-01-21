f1bce622576fe8256c19ff05f3d08d9393b029c9
mfrancis2337 <58670911+mfrancis2337@users.noreply.github.com>
2020-01-20 21:21:30 -0500

    Shooter Comments
    - Adds in a lot of comments to Shooter.java and shootBall.java
    - Simplifies a bit of code for putting current limits on the shooter motors

--- src/main/java/frc/robot/commands/Shooter/shootBall.java
+++ src/main/java/frc/robot/commands/Shooter/shootBall.java
@@ -8,7 +8,7 @@
 package frc.robot.commands.Shooter;
 
 import frc.robot.subsystems.Shooter;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /**
@@ -16,16 +16,23 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
  * @author Michael Francis
  */
 public class shootBall extends CommandBase {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  //These variables are used to take a parameter through the rest of the command in methods
   private final Shooter m_subsystem;
   private double m_modifier;
 
   /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
+   * Shoots the ball at a specified speed.
+   * @param subsystem
+   * The subsystem that the command uses (Shooter)
+   * @param modifier
+   * The velocity in which the shooter will shoot at.
+   * <ul>
+   * <li>From the initiation line, this number will be around 20000-22000.</li>
+   * <li>There is commented out code for this being controlled on SmartDashboard instead.</li>
+   * </ul>
    */
   public shootBall(Shooter subsystem, double modifier) {
+    //Puts the parameters in the command's variables to be used around as a shortcut.
     m_subsystem = subsystem;
     m_modifier = modifier;
     // SmartDashboard.putNumber("Shooter Speed", 17000);
@@ -36,6 +43,8 @@ public class shootBall extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    //Configures a closed-loop ramp of 0.5. This is necessary because of code in execute.
+    //The code in execute allows the shooter to coast after turning off.
     m_subsystem.shootMotor1.configClosedloopRamp(0.5);
     m_subsystem.shootMotor2.configClosedloopRamp(0.5);
     // m_modifier = SmartDashboard.getNumber("Shooter Speed", 17000);
@@ -45,6 +54,8 @@ public class shootBall extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    //Once the velocity reaches a speed of 5000, the closed-loop ramp is turned off.
+    //This is to ensure that the motors when turned off will coast their way down.
     if(m_subsystem.shootMotor1.getSelectedSensorVelocity() > 5000){
       m_subsystem.shootMotor1.configClosedloopRamp(0);
       m_subsystem.shootMotor2.configClosedloopRamp(0);
@@ -54,7 +65,10 @@ public class shootBall extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    // m_subsystem.stopShooter();
+    //Code that when the command ends, stop the shooter.
+    //Comment the code out if the ramp rate code in execute does not work or
+    //if you need to test.
+    m_subsystem.stopShooter();
   }
 
   // Returns true when the command should end.
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -27,85 +27,104 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  * @author Michael Francis
  */
 public class Shooter extends SubsystemBase {
-
+  //Creates motors
   public TalonFX shootMotor1;
   public TalonFX shootMotor2;
-
+  //Configures sensors
   public TalonFXConfiguration FXConfig;
-
-  private StatorCurrentLimitConfiguration currentLimitConfigurationMotor1 = new StatorCurrentLimitConfiguration();
-  private StatorCurrentLimitConfiguration currentLimitConfigurationMotor2 = new StatorCurrentLimitConfiguration();
+  //Configures code for putting limits onto motors
+  private StatorCurrentLimitConfiguration currentLimitConfigurationMotor = new StatorCurrentLimitConfiguration();
 
   /**
    * Shoots the ball with a certain strength
    */
   public Shooter() {
-    //Creates motors
+    //Also creates motors
     shootMotor1 = new TalonFX(0);
     shootMotor2 = new TalonFX(1);
-
+    //Also configures sensors
     FXConfig = new TalonFXConfiguration();
 
-    //Configs motors to factory default and configures their sensors
+    /** --- CONFIGURE MOTOR AND SENSOR SETTINGS --- **/
+    //Configures motors to factory default
     shootMotor1.configFactoryDefault();
     shootMotor2.configFactoryDefault();
+    //Configures sensors for PID calculations
     shootMotor1.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 0);
     shootMotor2.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 0);
-    
     FXConfig.primaryPID.selectedFeedbackSensor = FeedbackDevice.IntegratedSensor;
 
-    // Use current limit for each motor
-    currentLimitConfigurationMotor1 .currentLimit = 50;
-    currentLimitConfigurationMotor1 .enable = true;
-    currentLimitConfigurationMotor1 .triggerThresholdCurrent = 40;
-    currentLimitConfigurationMotor1 .triggerThresholdTime = 3;
-    currentLimitConfigurationMotor2 .currentLimit = 50;
-    currentLimitConfigurationMotor2 .enable = true;
-    currentLimitConfigurationMotor2 .triggerThresholdCurrent = 40;
-    currentLimitConfigurationMotor2 .triggerThresholdTime = 3;
-    // Set amperage limits
-    shootMotor1.configStatorCurrentLimit(currentLimitConfigurationMotor1, 0);
-    shootMotor2.configStatorCurrentLimit(currentLimitConfigurationMotor2, 0);
+    /** --- SETS UP SETTINGS (Such as current limits) ON MOTORS AND SENSORS --- **/
+    //Set up limits
+    currentLimitConfigurationMotor .currentLimit = 50;
+    currentLimitConfigurationMotor .enable = true;
+    currentLimitConfigurationMotor .triggerThresholdCurrent = 40;
+    currentLimitConfigurationMotor .triggerThresholdTime = 3;
+    //Implements these limits on the motors
+    shootMotor1.configStatorCurrentLimit(currentLimitConfigurationMotor, 0);
+    shootMotor2.configStatorCurrentLimit(currentLimitConfigurationMotor, 0);
 
-    //Configs PIDs
+    /** --- CONFIGURE PIDS --- **/
+    //Set variables
     final double kP = 0.25;
     final double kI = 0;
+    //Implement variables into the PIDs
     shootMotor1.config_kP(0, kP);
     shootMotor1.config_kI(0, kI);
+
     shootMotor2.config_kP(0, kP);
     shootMotor2.config_kI(0, kI);
+    //Set a closed-loop ramp rate
     shootMotor1.configClosedloopRamp(0.5);
     shootMotor2.configClosedloopRamp(0.5);
-
+    //Makes sure the robot recognizes that it needs to use voltage stuff
     shootMotor1.enableVoltageCompensation(true);
     shootMotor2.enableVoltageCompensation(true);
 
-    // Configs other information
+    /** --- OTHER MOTOR INFORMATION SET UP --- **/
+    //Sets up brakes
     shootMotor1.setNeutralMode(NeutralMode.Coast);
     shootMotor2.setNeutralMode(NeutralMode.Coast);
+    //Sets up inversions
     shootMotor1.setInverted(true);
     shootMotor2.setInverted(false);
     // shootMotor.configSelectedFeedbackSensor(feedbackDevice, pidIdx, timeoutMs)
   }
 
+  /**
+   * Boolean that returns true when the shooter temperature is over 70 degrees Celsius.
+   * <p>Used in the periodic of Shooter.java for SmartDashboard</p>
+   */
+  public boolean shooterTemp;
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    //This code here puts things on the Smart Dashboard so that they may be read as we drive the robot.
     SmartDashboard.putNumber("Shooter Motor 1 Velocity", shootMotor1.getSelectedSensorVelocity());
     SmartDashboard.putNumber("Shooter Motor 2 Velocity", shootMotor2.getSelectedSensorVelocity());
     SmartDashboard.putNumber("Shooter Motor 1 Temperature", shootMotor1.getTemperature());
     SmartDashboard.putNumber("Shooter Motor 2 Temperature", shootMotor2.getTemperature());
-    SmartDashboard.putBoolean("Is Either Motor Above 70C",
-        shootMotor1.getTemperature() > 70 || shootMotor2.getTemperature() > 70);
+
+    //Variable that returns true when the one of the motors of the shooter are over 70 degrees Celsius
+    shooterTemp = shootMotor1.getTemperature() > 70 || shootMotor2.getTemperature() > 70;
+    //This sets up a rectangle on the Smart Dashboard that turns green when shooterTemp returns true (see above)
+    SmartDashboard.putBoolean("Is Either Motor Above 70C", shooterTemp);
   }
 
-  public void setShooterSpeed(final double velo) {
+  /**
+   * Sets the shooter motors to run at a certain speed
+   * @param velo
+   * The <em>velo</em>city at which the motors run at
+   */
+  public void setShooterSpeed(double velo) {
     //if (shootMotor1.getTemperature() < 70 && shootMotor2.getTemperature() < 70) {
       shootMotor1.set(ControlMode.Velocity,  velo);//velo * 4096 / 600);
       shootMotor2.set(ControlMode.Velocity,  velo);//velo * 4096 / 600);
     //}
   }
 
+  /**
+   * Stops the shooter motors by setting their velocity to 0
+   */
   public void stopShooter() {
     shootMotor1.set(TalonFXControlMode.Velocity, 0);
     shootMotor2.set(TalonFXControlMode.Velocity, 0);
