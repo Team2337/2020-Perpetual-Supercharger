909e017a8f3b07dac8f6490982302dd736e767ff
brycegerman <r2bryce2@gmail.com>
2020-01-22 15:02:02 -0500

    Added explaination to yaw

--- src/main/java/frc/robot/subsystems/Pigeon.java
+++ src/main/java/frc/robot/subsystems/Pigeon.java
@@ -33,8 +33,6 @@ public class Pigeon extends SubsystemBase {
 		gyroGenStatus = new PigeonIMU.GeneralStatus();
 		ypr_deg = new double[3];
 		xyz_dps = new double[3];
-
-		// pidgey.enterCalibrationMode(CalibrationMode.Magnetometer360);
 	}
 		
 
@@ -48,7 +46,6 @@ public class Pigeon extends SubsystemBase {
 		pidgey.getYawPitchRoll(ypr_deg);
 		pidgey.getRawGyro(xyz_dps);
 
-		
 		//SmartDashboard.putNumber("FusedHeading", pidgey.getFusedHeading());
 		SmartDashboard.putNumber("yaw", getYaw());
 		//SmartDashboard.putNumber("Pitch", getPitch());
@@ -57,6 +54,19 @@ public class Pigeon extends SubsystemBase {
 
 	/**
 	 * Gets the yaw from the gyro
+	 * The yaw is the angle of the robot in the Z axis, 
+	 * it has some historical connotations from where this value's name comes from:
+	 *  --- History ---
+	 * In the heyday of large sailing ships, numerous nautical words appeared on the horizon, 
+	 * many of which have origins that have never been traced. "Yaw" is one such word. 
+	 * It began showing up in print in the 16th century, first as a noun 
+	 * (meaning "movement off course" or "side to side movement") and then as a verb. 
+	 * For more than 350 years it remained a sailing word, with occasional side trips 
+	 * to the figurative sense "to alternate." 
+	 * Then dawned the era of airplane flight in the early 20th century, 
+	 * and "yawing" was no longer confined to the sea. Nowadays, 
+	 * people who love boats still use "yaw" much as did the sailing-men of old, 
+	 * but pilots and rocket scientists also refer to the "yawing" of their crafts.
 	 * @return yaw
 	 */
 	public double getYaw() {
@@ -112,12 +122,13 @@ public class Pigeon extends SubsystemBase {
 	
 	/**
 	 * Use to manually set the yaw in degrees
+	 * 
 	 */
 	public void manualSetYaw(double yaw) {
 		yaw *= 64;
 		pidgey.setYaw(yaw, timeoutMs);
-
 	}
+
 	 /**
 	 * Returns the FusedHeading value from the gyro
 	 * @return FusedHeading
