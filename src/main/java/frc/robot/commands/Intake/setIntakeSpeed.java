8242c3d808d62dcc4b6a62df528a9f886bd8b3ea
Michael Francis <58670911+mfrancis2337@users.noreply.github.com>
2020-01-25 13:04:01 -0500

    Quick Fix
    Forgot to delete a line commented out line in a command

--- src/main/java/frc/robot/commands/Intake/setIntakeSpeed.java
+++ src/main/java/frc/robot/commands/Intake/setIntakeSpeed.java
@@ -42,7 +42,6 @@ public class setIntakeSpeed extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    // m_subsystem.setIntakeSpeed(intakeSpeed);
   }
 
   // Called once the command ends or is interrupted.
