fbf180a1143d9a862b257de21bebda157c5450e3
Michael Francis <58670911+mfrancis2337@users.noreply.github.com>
2020-01-25 20:11:36 -0500

    Added in test results
    Copied over test results into a txt file for reference

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -6,6 +6,7 @@ import frc.robot.commands.Shooter.shootBall;
 public class OI {
     public NerdyXbox joystick = new NerdyXbox(0);
     public OI(){
+        //See src\main\java\frc\robot\OI_test_results.txt for values
         joystick.triggerRight .whileHeld(new shootBall(Robot.Shooter, 15295));//16120
     }
 }
