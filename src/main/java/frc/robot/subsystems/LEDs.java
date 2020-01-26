ba5c786e93f173c13e544cd2878a9a022cb085b3
Nicholas-Stokes <nicholasbstokes56@gbstu.org>
2020-01-26 17:13:05 -0500

    Made a LED Branch
    Just for upkeep and organization

--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -11,7 +11,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LEDs extends SubsystemBase {
   /**
-   * Creates a new ExampleSubsystem.
+   * Creates a new LED subsystem.
    */
   public LEDs() {
 
