bdffb09618db99d7f58522d40e863801f4441450
johnhasnocluehow <46582394+johnhasnocluehow@users.noreply.github.com>
2020-01-27 21:00:22 -0500

    Fixed comments in Left hopper

--- src/main/java/frc/robot/commands/Hopper/leftHopperExtension.java
+++ src/main/java/frc/robot/commands/Hopper/leftHopperExtension.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.commands.Hopper;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
@@ -30,14 +23,16 @@ public class leftHopperExtension extends CommandBase {
     subsystem.extendLeftFlipper(true);
   }
 
-  // Called once the command ends or is interrupted.
+  /*
+   * Called once the command ends or is interrupted. When interupted, retract
+   * the left hopper
+   */
   @Override
-  // Retracts left flipper
   public void end(boolean interrupted) {
     subsystem.extendLeftFlipper(false);
   }
 
-  // Returns true when the command should end.
+  // Retracts left flipper
   @Override
   public boolean isFinished() {
     return false;
--- src/main/java/frc/robot/commands/Hopper/rightHopperExtension.java
+++ src/main/java/frc/robot/commands/Hopper/rightHopperExtension.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.commands.Hopper;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
@@ -32,14 +25,13 @@ public class rightHopperExtension extends CommandBase {
 
   /*
    * Called once the command ends or is interrupted. When interupted, retract
-   * right hopper
+   * the right hopper
    */
   @Override
   public void end(boolean interrupted) {
     subsystem.extendRightFlipper(false);
   }
 
-  // Returns true when the command should end.
   @Override
   // Retracts right flipper
   public boolean isFinished() {
