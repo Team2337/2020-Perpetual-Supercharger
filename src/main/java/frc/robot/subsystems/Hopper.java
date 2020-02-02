f0a18b152e58854ea223c12144178d45e0c48626
johnhasnocluehow <46582394+johnhasnocluehow@users.noreply.github.com>
2020-02-02 16:21:27 -0500

    Perams added

--- src/main/java/frc/robot/subsystems/Hopper.java
+++ src/main/java/frc/robot/subsystems/Hopper.java
@@ -21,17 +21,19 @@ public class Hopper extends SubsystemBase {
     leftFlipper = new Solenoid(Constants.LEFTFLIPPERSOLENOID);
     rightFlipper = new Solenoid(Constants.RIGHTFLIPPERSOLENOID);
   }
+  
   /**
-   *  Defines the act of extending and retracting the left flipper through a
-   *  boolean state
-  **/ 
+   * Defines the act of extending and retracting the left flipper through a boolean state
+   * @param state This statement makes the left flipper of the hopper extend or retract based on a boonlean state of wheather or not the button is pressed or not. 
+   */
   public void extendLeftFlipper(boolean state) {
     leftFlipper.set(state);
   }
- /**
-  * Defines the act of extending and retracting the right flipper through a
-  * boolean state
- **/
+   
+  /**
+   * Defines the act of extending and retracting the right flipper through a boolean state
+   * @param state This statement makes the right flipper of the hopper extend or retract based on a boonlean state of wheather or not the button is pressed or not. 
+   */
   public void extendRightFlipper(boolean state) {
     rightFlipper.set(state);
   }
