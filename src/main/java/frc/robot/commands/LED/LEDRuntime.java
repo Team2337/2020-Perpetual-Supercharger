4ab9a9da18055f6353383ef4514de9d5c07e8cc6
Nicholas-Stokes <nicholasbstokes56@gbstu.org>
2020-02-23 14:43:26 -0500

    Update LEDRuntime.java

--- src/main/java/frc/robot/commands/LED/LEDRuntime.java
+++ src/main/java/frc/robot/commands/LED/LEDRuntime.java
@@ -1,8 +1,6 @@
 package frc.robot.commands.LED;
 
-import edu.wpi.first.wpilibj.smartdashboard.SendableRegistry;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Robot;
 import frc.robot.subsystems.LED;
 
 /**
@@ -14,7 +12,6 @@ import frc.robot.subsystems.LED;
 public class LEDRuntime extends CommandBase {
 
 	public LEDRuntime() {
-		SendableRegistry.addChild(this, Robot.LEDs);
 	}
 
 	public  void initialize() {
@@ -22,28 +19,6 @@ public class LEDRuntime extends CommandBase {
 	}
 
 	public void execute() {
-			/**if(Robot.HatchBeak.status()) {
-				LED.setColor(LED.white);
-			} else {
-				if(Robot.ClimberDeploy.climberPhase < 5) {
-					if(autoEndAuto.endedAutoLED) {
-						LED.setColor(LED.red);
-					} 
-					if(!Robot.ClimberDeploy.climberLineSensor.get() || Robot.ClimberDeploy.climberPhase == 3) {
-						LED.setColor(LED.green);
-					} else {
-						if(Robot.ClimberDeploy.getServo() == 0.8) {
-							LED.setColor(LED.darkBlue);
-						} else {
-							LED.setColor(LED.rainbow);
-						}
-					}
-				} else {
-					if(Robot..getServo() == 0.8) {
-						LED.setColor(LED.blue);
-					} else {
-						LED.setColor(LED.off);
-					}*/
 				}
 
 	public boolean isFinished() {
