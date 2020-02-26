e5a32f9f166f3a376bae041ca762bc5f1760955f
brycegerman <r2bryce2@gmail.com>
2020-02-26 18:33:07 -0500

    added code to turn on led strip when shooter is on

--- src/main/java/frc/robot/commands/LED/LEDRuntime.java
+++ src/main/java/frc/robot/commands/LED/LEDRuntime.java
@@ -2,27 +2,45 @@ package frc.robot.commands.LED;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
-import frc.robot.subsystems.LED;
+import frc.robot.subsystems.LEDs;
 
 /**
- * Sets the color according to various factors, such as if the robot was
- * intaking, climbing, etc.
- * 
- * @author Zayd A. Jack E.
+ * Turns the LED strip on or off depending on if the shooter is running.
+ * This will also flash the LEDs on and off, if the intake is running 
+ * while the shooter is off
+ * @author Bryce G.
  */
 public class LEDRuntime extends CommandBase {
-	private final LED led;
+	private final LEDs led;
+	private int iterations = 0;
+	private int lastState = 0;
 
-	public LEDRuntime(LED led) {
+	public LEDRuntime(LEDs led) {
 		this.led = led;
 		addRequirements(led);
 	}
 
 	public void initialize() {
-		Robot.LED.setColor(Robot.LED.off);
+		Robot.LEDs.turnOffLEDs();
 	}
 
 	public void execute() {
+		if(Robot.Shooter.shooterAtVelocity) {
+			Robot.LEDs.turnOnLEDs();
+		} else if(Robot.Intake.getIntakeSpeed() > 0) {
+			if(iterations % 50 == 0) {
+				if(lastState == 0) {
+					Robot.LEDs.turnOnLEDs();
+					lastState = 1;
+				} else {
+					Robot.LEDs.turnOffLEDs();
+					lastState = 0;
+				}
+			}
+			iterations++;
+		} else {
+			Robot.LEDs.turnOffLEDs();
+		}
 	}
 
 	public boolean isFinished() {
