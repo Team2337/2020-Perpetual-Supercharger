4d008713b9c6765e47fa5f248f4daf6f15001345
brycegerman <r2bryce2@gmail.com>
2020-02-26 21:19:02 -0500

    Uncommented code

--- src/main/java/frc/robot/commands/auto/AutoRotateWithVision.java
+++ src/main/java/frc/robot/commands/auto/AutoRotateWithVision.java
@@ -42,13 +42,11 @@ public class AutoRotateWithVision extends CommandBase {
 
   @Override
   public void execute() {
-    /* if(Robot.Vision.getPipeline() == 0) {
+    if(Robot.Vision.getPipeline() == 0) {
       rotation = -(Math.toRadians(Robot.Vision.getDoubleValue("tx")) * Constants.VISIONCLOSEROTATIONP);
     } else {
       rotation = -(Math.toRadians(Robot.Vision.getDoubleValue("tx")) * Constants.VISIONFARROTATIONP);
-    } */
-    Robot.OperatorAngleAdjustment.setLimelightRotationMode(true);
-    rotation = -(Math.toRadians(Robot.Vision.getDoubleValue("tx")) * Constants.VISIONFARROTATIONP);
+    } 
     
     // Pass on joystick values to be calculated into angles and speeds
     Robot.SwerveDrivetrain.calculateJoystickInput(0, 0, rotation);
@@ -58,7 +56,6 @@ public class AutoRotateWithVision extends CommandBase {
   public void end(boolean interrupted) {
     Robot.OperatorAngleAdjustment.setLimelightRotationMode(false);
     Robot.OperatorAngleAdjustment.setOffsetAngle(-Robot.Utilities.getPigeonYawMod());
-    Robot.OperatorAngleAdjustment.setLimelightRotationMode(false);
   }
 
   @Override
